c63f7aa46ef4cadd89b9ec147ef246e85d081f65
qwertyjack12 <zelenin.2016@bk.ru>
2023-02-24 17:20:38 +1000

    lr2 new documentation of class User

--- src/Students/User.java
+++ src/Students/User.java
@@ -1,36 +1,81 @@
 package Students;
 
-public class User extends AbstractUser{
+/**
+ * Класс пользователя
+ *
+ * @author Zelenin
+ * @version 1.1
+ */
+public class User extends AbstractUser {
+    /**
+     * Конструктор - создание нового объекта с определенными значениями
+     *
+     * @param fio - фио пользователя
+     * @param age - кол-во лет пользователя
+     * @see User#User(String, Integer)
+     */
     public User(String fio, Integer age) {
         this.fio = fio;
         this.age = age;
     }
 
+    /**
+     * Переопределенный метод toString() базового класса Object
+     */
     @Override
     public String toString() {
         return "Пользователь: " + fio + " , которому " + age + " лет";
     }
 }
 
+/**
+ * Абстрактный класс пользователя со свойствами fio, age
+ *
+ * @author Zelenin
+ */
+
 abstract class AbstractUser {
+    /**
+     * Поле фио
+     */
     String fio;
+
+    /**
+     * Поле количество лет
+     */
     Integer age;
 
+    /**
+     * Метод определения fio {@link User#fio}
+     */
     public void setFio(String fio) {
         this.fio = fio;
     }
 
+    /**
+     * Метод определения age {@link User#age}
+     */
     public void setAge(Integer age) {
         this.age = age;
     }
 
+    /**
+     * Метод получения значения поля fio {@link User#fio}
+     */
     public String getFio() {
         return this.fio;
     }
 
+    /**
+     * Метод получения значения поля age {@link User#age}
+     */
     public Integer getAge() {
         return this.age;
     }
 
+    /**
+     * Абстрактный метод для переопределения метода toString{@link User#toString()}
+     * базового класса Object
+     */
     public abstract String toString();
 }
